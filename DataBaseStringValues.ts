3f715f863f1241f7b7e183a1e32b9e7cbb4554f5
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2021-12-28 17:44:57 -0500

    Update DataBaseStringValues.ts

--- DataBaseStringValues.ts
+++ DataBaseStringValues.ts
@@ -1,14 +1,9 @@
 
 const process = require('process');
 
-const LocalSource:boolean = false;
 
 export function GetPrimaryConnectionKey():string{
-    if(LocalSource)
         return String(process.env.CD_PK);
-    else
-        return String(CD_PK);
-    
 }
 
 export function GetDataBaseName():string{
